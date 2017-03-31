86b16d89380bdea7facc84a352bf0edaf813d6b3
Vaden Masrani <vadmas@gmail.com>
2017-03-31 14:27:00 -0700

    Added multiselect

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ var renderBubbles = function(container_id, data, mouseClickCallback){
 	  });
 	};
 
+	d3.selection.prototype.moveToBack = function() { 
+    return this.each(function() { 
+        var firstChild = this.parentNode.firstChild; 
+        if (firstChild) { 
+            this.parentNode.insertBefore(this, firstChild); 
+        } 
+    }); 
+};
+
 	var bbox = d3.select(container_id).node().getBoundingClientRect()
 	var width = bbox.width
 	var height = bbox.height
@@ -27,53 +36,52 @@ var renderBubbles = function(container_id, data, mouseClickCallback){
 	    .enter().append("g")
 	      .attr("class", "node")
 	      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-	        .on("click", function(d,i) {
-	         	var node = this;
-	         	var d3Node = d3.select(node);
+	        .on("click", function(d,i,evt) {
+	        	var node = this;
+	         	var selectedNode = d3.select(node);
 
-	         	// If node is deselected
-	         	if(d3Node.classed('active')){
-					// Reset all nodes opacity
-					d3.selectAll('.node')
-	         			.classed('active', false)
-	         			.transition()
-	         			.ease(d3.easeElastic)
-	         			.delay(0)
-	         			.duration(800)
-	         			.style('opacity',1);
-	     			// Reset node size
-	         		d3Node.select('circle').transition()
-	         			.ease(d3.easeElastic)
-	         			.delay(0)
-	         			.duration(800)
-	         			.attr('r',function(d){return d.r})
-		         }
-		         // If node is selected for first time
-	         	else{
-					// Move node to front 
-	         		d3Node.moveToFront()
-	         		// Fade out all other nodes
-	         		d3.selectAll('.node')
-	         			.classed('active', false)
-	         			.transition()
-	         			.ease(d3.easeElastic)
-	         			.delay(0)
-	         			.duration(800)
-	         			.style('opacity',function () {
-				        	return (this === node) ? 1.0 : 0.4;
-	    				});
-    				// Change selected node to 'active'
-	    			d3Node.classed('active', true)
-	    			// Enlarge selected node, shrink all others
-	         		d3.selectAll('circle').transition()
-	         			.ease(d3.easeElastic)
-	         			.delay(0)
-	         			.duration(800)
-	         			.attr('r',function (d) {
-				  		      return (this.parentNode === node) ? d.r * 1.05 : d.r;
-			  		  })
-		         }
-		     })
+	         	// If shift key not pressed and user selects an active node, 
+	         	// restore to default
+        		if (!d3.event.shiftKey && d.is_active) {
+        			reset_all()
+        			restore_opacity()
+        			restore_size()
+        			return
+        		}
+
+				// If shift is not held and user selects an inactive node set.
+		        // clear all active nodes
+	        	if (!d3.event.shiftKey && !d.is_active) {
+	        		reset_all()
+	        	}
+
+	        	// This allows deselection if shift is held
+	        	if (d.is_active){
+	        		d.is_active = false;	
+	        		selectedNode.moveToBack()
+	        	} 
+	        	else{
+	        		d.is_active = true;	
+	        		selectedNode.moveToFront()
+	        	}
+
+         		// Fade out all other nodes
+         		d3.selectAll('.node')
+         			.transition()
+         			.duration(130)
+         			.style('opacity',function (d) {
+         				return (d.is_active) ? 1 : 0.4
+    				});
+				
+         		d3.selectAll('circle').transition()
+         			.ease(d3.easeElastic)
+         			.delay(0)
+         			.duration(800)
+         			.attr('r',function (d) {
+         				return (d.is_active ) ? d.r * 1.05 : d.r;
+			  		 })
+			     })
+
 	        // Fire user event
 	        .on("click.user", mouseClickCallback)
 
@@ -96,7 +104,29 @@ var renderBubbles = function(container_id, data, mouseClickCallback){
 	     .style("font-size", function(d) {return Math.max(10, d.r/4) + "px"})
 	     .text(function(d) {
 	       return d.data.country;
-	      })
+	  })
+
+
+	 function reset_all(){
+	 	d3.selectAll('.node').each(function(dd){dd.is_active = false})
+	 }
+
+	 function restore_opacity(){
+	 	d3.selectAll('.node')
+ 			.transition()
+ 			.duration(130)
+ 			.style('opacity',1);
+	 }
+	
+	function restore_size(){
+	 	d3.selectAll('circle').transition()
+ 			.ease(d3.easeElastic)
+ 			.delay(0)
+ 			.duration(800)
+ 			.attr('r',function (d){return d.r;})
+     }
+
+     
 }
 // ================================================================================
 // ------------------------------------Demo----------------------------------------
@@ -108,14 +138,14 @@ var data = {
     {z: 13.8, name: 'BE',country: 'Belgium'},
     {z: 4.7, name: 'DE',country: 'Germany'},
     {z: 15.8, name: 'FI',country: 'Finland'},
-    // {z: 12, name: 'NL',country: 'Netherlands'},
-    // {z: 11.8, name: 'SE',country: 'Sweden'},
-    // {z: 16.6, name: 'ES',country: 'Spain'},
-    // {z: 14.5, name: 'FR',country: 'France'},
-    // {z: 10, name: 'NO',country: 'Norway'},
-    // {z: 204.7, name: 'UK',country: 'UnitedKingdom'},
-    // {z: 10.4, name: 'IT',country: 'Italy'},
-    // {z: 16, name: 'RU',country: 'Russia'},
+    {z: 12, name: 'NL',country: 'Netherlands'},
+    {z: 11.8, name: 'SE',country: 'Sweden'},
+    {z: 16.6, name: 'ES',country: 'Spain'},
+    {z: 14.5, name: 'FR',country: 'France'},
+    {z: 10, name: 'NO',country: 'Norway'},
+    {z: 204.7, name: 'UK',country: 'UnitedKingdom'},
+    {z: 10.4, name: 'IT',country: 'Italy'},
+    {z: 16, name: 'RU',country: 'Russia'},
     {z: 35.3, name: 'US',country: 'UnitedStates'},
     {z: 28.5, name: 'HU',country: 'Hungary'},
     {z: 15.4, name: 'PT',country: 'Portugal'},
